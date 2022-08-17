481edfa138ed4da0185cd5ad7cc491300303052e
igorhit <igor_hit@yahoo.com.br>
2022-08-16 22:43:30 -0300

    Add files via upload

---
+++ 128c - moduloClient.js
@@ -0,0 +1,11 @@
+const moduloA = require('./128a - moduloA')
+const moduloB = require('./128b - moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
